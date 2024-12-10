0b91469187d37150791f7def649f7439846ca561
leonj <jakleb20@htl-kaindorf.at>
2024-12-10 15:55:59 +0100

    cypress fronend

--- frontend/cypress.config.cjs
+++ frontend/cypress.config.cjs
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cypress_1 = require("cypress");
 exports.default = (0, cypress_1.defineConfig)({
     e2e: {
-        baseUrl: 'http://95.143.172.216:45922/', // Ihre spezifische URL
+        port: 45922,
         supportFile: false, // Optional, falls Sie das support File deaktivieren möchten
     },
 });
