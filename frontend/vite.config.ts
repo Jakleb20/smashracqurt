6ecc9f7dd16d77f3c6478067bc2a052ff380752e
leonj <jakleb20@htl-kaindorf.at>
2024-12-10 14:04:43 +0100

    a

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Ändere den Host hier
     port: 45921, // Ändere den Port hier
   },
 })
