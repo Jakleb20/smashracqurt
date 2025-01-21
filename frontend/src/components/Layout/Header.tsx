dd21c56909033caa0ee17c02c32bd037bbe1bbb4
leonj <jakleb20@htl-kaindorf.at>
2025-01-21 16:11:27 +0100

    header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div>
             <nav className="navbar">
                 <div className="left-section">
-                    <h1>Paulus ist cool</h1>
+                    <h1>SmashRacquet</h1>
                     <br/>
                 </div>
                 <div className="center-section">
