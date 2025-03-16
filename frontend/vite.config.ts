f4475dcb7d19c05f0834a00b1f868a39ec371595
jakleb20 <jakleb20@htl-kaindorf.at>
2025-03-16 19:35:06 +0100

    .env file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+// Lade die .env-Datei
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Ändere den Host hier
-    port: 45921, // Ändere den Port hier
+    host: process.env.VITE_HOST || '0.0.0.0', // Verwende den Wert aus .env oder einen Fallback
+    port: parseInt(process.env.VITE_PORT || '45920', 10), // Verwende den Wert aus .env oder einen Fallback
   },
-})
+});
